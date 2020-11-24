f5da9786718d8d8d6c9900a1282aab3461fc9a52
Giancarlo A. Antonucci <antonuccig@icloud.com>
2020-11-24 16:59:04 +0000

    Solved an issue with design

--- src/ArrowMacros.jl
+++ src/ArrowMacros.jl
@@ -2,13 +2,22 @@ module ArrowMacros
 
 export @↓, @↑, @⤓, @⤒, @←
 
-function _prepend_onelevel!(ex, constr)
+function _getfield(constr, name)
+    if isstructtype(typeof(constr))
+        if isdefined(constr, name)
+            return getfield(constr, name)
+        end
+    end
+    return name
+end
+
+function _prepend!(ex, constr)
     for (i, ex_i) in enumerate(ex.args)
         if ex_i isa Symbol
             ex_i_string = String(ex_i)
             t = gensym()
             ex.args[i] = quote
-                $t = $getfield($constr, Symbol($ex_i_string))
+                $t = $_getfield($constr, Symbol($ex_i_string))
                 if $t isa Symbol
                     $t = $ex_i
                 end
@@ -39,7 +48,7 @@ macro ↓(input)
         end
         name_ = string(name)
         if name isa Symbol
-            push!(output.args, :($value = $getfield($constr, Symbol($name_))))
+            push!(output.args, :($value = $_getfield($constr, Symbol($name_))))
         elseif name isa Expr
             ex = name
             _prepend!(ex, constr)
@@ -49,14 +58,14 @@ macro ↓(input)
     esc(output)
 end
 
-function _getfield_nested(constr, name)
+function _deep_getfield(constr, name)
     if isstructtype(typeof(constr))
         if isdefined(constr, name)
             return getfield(constr, name)
         else
             for fn in fieldnames(typeof(constr))
                 field = getfield(constr, fn)
-                value = _getfield_nested(field, name)
+                value = _deep_getfield(field, name)
                 if value != name
                     return value
                 end
@@ -66,20 +75,20 @@ function _getfield_nested(constr, name)
     return name
 end
 
-function _prepend!(ex, constr)
+function _deep_prepend!(ex, constr)
     for (i, ex_i) in enumerate(ex.args)
         if ex_i isa Symbol
             ex_i_string = String(ex_i)
             t = gensym()
             ex.args[i] = quote
-                $t = $_getfield_nested($constr, Symbol($ex_i_string))
+                $t = $_deep_getfield($constr, Symbol($ex_i_string))
                 if $t isa Symbol
                     $t = $ex_i
                 end
                 $t
             end
         elseif ex_i isa Expr
-            ex.args[i] = _prepend!(ex_i, constr)
+            ex.args[i] = _deep_prepend!(ex_i, constr)
         end
     end
     return ex
@@ -103,14 +112,23 @@ macro ⤓(input)
         end
         name_ = string(name)
         if name isa Symbol
-            push!(output.args, :($value = $_getfield_nested($constr, Symbol($name_))))
+            push!(output.args, :($value = $_deep_getfield($constr, Symbol($name_))))
         elseif name isa Expr
             ex = name
-            _prepend!(ex, constr)
+            _deep_prepend!(ex, constr)
             push!(output.args, :($value = $ex))
         end
     end
     esc(output)
+end
+
+function _setfield!(constr, name, value)
+    if isstructtype(typeof(constr))
+        if isdefined(constr, name)
+            return setfield!(constr, name, value)
+        end
+    end
+    return name
 end
 
 """
@@ -130,19 +148,19 @@ macro ↑(input)
             name, value = block.args[2:3]
         end
         name_ = string(name)
-        push!(output.args, :($setfield!($constr, Symbol($name_), $value)))
+        push!(output.args, :($_setfield!($constr, Symbol($name_), $value)))
     end
     esc(output)
 end
 
-function _setfield_nested!(constr, name, value)
+function _deep_setfield!(constr, name, value)
     if isstructtype(typeof(constr))
         if isdefined(constr, name)
             return setfield!(constr, name, value)
         else
             for fn in fieldnames(typeof(constr))
                 field = getfield(constr, fn)
-                _setfield_nested!(field, name, value)
+                _deep_setfield!(field, name, value)
             end
         end
     end
@@ -166,7 +184,7 @@ macro ⤒(input)
             name, value = block.args[2:3]
         end
         name_ = string(name)
-        push!(output.args, :($_setfield_nested!($constr, Symbol($name_), $value)))
+        push!(output.args, :($_deep_setfield!($constr, Symbol($name_), $value)))
     end
     esc(output)
 end
